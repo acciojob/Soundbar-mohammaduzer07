241e96d3b4810f662223a5560675a5a1336d5927
mohammaduzer07 <134217616+mohammaduzer07@users.noreply.github.com>
2024-04-20 12:17:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+var audio;
+
+function playSound(soundFileName) {
+    if (audio) {
+        audio.pause();
+        audio.currentTime = 0;
+    }
+    audio = new Audio('sounds/' + soundFileName);
+    audio.play();
+}
+
+function stopSound() {
+    if (audio) {
+        audio.pause();
+        audio.currentTime = 0;
+    }
+}
